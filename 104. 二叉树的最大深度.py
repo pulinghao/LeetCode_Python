dc39220c53072135092b969b2dbb259302be49c9
pulinghao <536400571@qq.com>
2020-04-08 13:46:22 +0800

    树

---
+++ 104. 二叉树的最大深度.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time :    2020/4/8 12:49 下午
+@Author:  pulinghao
+@File: 104. 二叉树的最大深度.py
+@Software: PyCharm
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
--- 409. 最长回文串.py
+++ 409. 最长回文串.py
@@ -5,6 +5,9 @@
  @Author   :pulinghao@baidu.com
  @File     :最长回文串.py 
  @Description :
+ 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+
+ 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。
 """
 
 class Solution(object):
